1c140ef6f1aca74abf18fc1b6017ca708fa21233
Vikas Gavali <vikas.gavali@glabsoftech.com>
2023-06-08 09:32:22 +0530

    GL DTO and Classs

---
+++ Api/BookKeeper/BookKeeper.DTO/GeneralLedgerAccountDTO.cs
@@ -0,0 +1,29 @@
+﻿namespace BookKeeper.DTO
+{
+    public class GeneralLedgerAccountDTO
+    {
+        public Guid Id { get; set; }
+
+        public string GlNumber { get; set; }
+
+        public Guid JournalEntryId { get; set; }
+
+        public Guid AccountId { get; set; }
+
+        public Guid ExplanationId { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public string DrCr { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public virtual JournalEntryDTO JournalEntry { get; set; }
+
+        public virtual AccountDTO Account { get; set; }
+
+        public virtual AccountDTO Explanation { get; set; }
+    }
+}
--- Api/BookKeeper/BookKeeper.DTO/GeneralLedgerDTO.cs
+++ Api/BookKeeper/BookKeeper.DTO/GeneralLedgerDTO.cs
@@ -2,6 +2,16 @@
 {
     public class GeneralLedgerDTO
     {
+        public string Narration { get; set; }
+
+        public Guid AccountId { get; set; }
+
+        public Guid ExplanationId { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public DateTime Date { get; set; }
+
 
     }
 }
