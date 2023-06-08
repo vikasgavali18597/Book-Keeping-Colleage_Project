fb25c64ac016d6f976999eaaea837295fb34b9e0
Vikas Gavali <vikas.gavali@glabsoftech.com>
2023-06-08 10:18:30 +0530

    general ledger controller added

---
+++ Api/BookKeeper/BookKeeper.Api/Controllers/GeneralLedgerController.cs
@@ -0,0 +1,21 @@
+﻿using BookKeeper.Service.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookKeeper.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GeneralLedgerController : ControllerBase
+    {
+        private readonly IGeneralLedgerService _ledgerService;
+
+        public GeneralLedgerController(IGeneralLedgerService ledgerService)
+        {
+            _ledgerService = ledgerService;
+        }
+
+
+
+    }
+}
